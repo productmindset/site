1dfa8af1be94de7841436111b7b925fc117a7827
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-03-14 02:45:54 -0400

    reverted cms.tsx to cms.js.  Added repo back to netlify cms config.yml

---
+++ content/pages/page-1.md
@@ -0,0 +1,8 @@
+---
+path: /page-1
+title: Page 1
+description: The first page in the pages collection
+---
+### Page 1
+
+The first page in the pages collection.
--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.tsx`,
+        modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     `gatsby-transformer-remark`,

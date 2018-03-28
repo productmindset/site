45e5284cf234b5eecc438e4f34ec3678b70c8769
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-03-28 15:44:56 -0400

    addess pr comments, clean up commented out code and uneeded changes

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -24,12 +24,4 @@ const typography = new Typography({
     // See below for the full list of options.
 })
 
-// Output CSS as string.
-// mtypography.toString()
-
-// Or insert styles directly into the <head> (works well for client-only
-// JS web apps.
-// typography.injectStyles()
-// })
-
 export default typography

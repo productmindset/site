42a0a742bab6f11da1e269306228592fcc5e1789
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-04-01 12:20:26 -0400

    use lodash to split speaker list into cunks of two speakers for column formatting

--- content/speakers/Rivers.md
+++ content/speakers/Rivers.md
@@ -7,7 +7,7 @@ fulllName: Jonathan Rivers
 jobTitle: Chief Technology Officer
 company: 3Pillar Global
 description: >-
-  Jonathan Rivers is the CTO of 3Pillar Global, where he leads 3Pillar's Procut and Engineering organizations.
+  Jonathan Rivers is the CTO of 3Pillar Global, where he leads 3Pillar's Product and Engineering organizations.
   He works with 3Pillar teams in each of our locations to foster product development best practices that lead to business value for clients.
 image: /images/Rivers.png
 ---
--- src/templates/workshops-page.tsx
+++ src/templates/workshops-page.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as _ from 'lodash'
 import * as Page from './page'
 import * as PageComponent from '../components/PageComponent'
 import { MarkdownRemark, MarkdownRemarkConnection, ImageSharp } from '../graphql-types'
@@ -16,26 +17,31 @@ const WorkshopsPageLayout: React.StatelessComponent<WorkshopPageTemplateLayoutPr
     <PageComponent.default {...props}>
       <div className="section container" >
         <h3 className="title">Speakers</h3>
-        {props.data!.speakers!.edges!.map((node) => (
-          <div className="card">
-            <div className="card-content">
-              <div className="media">
-                <div className="media-left">
-                  <figure className="image is-128x128">
-                    <img src={node.node!.frontmatter!.image!} alt={node.node!.frontmatter!.fulllName!} />
-                  </figure>
-                </div>
-                <div className="media-content">
-                  <p className="title is-4">{node.node!.frontmatter!.fulllName!}</p>
-                  <p className="subtitle is-6">{node.node!.frontmatter!.jobTitle!}</p>
-                  <p className="subtitle is-6">{node.node!.frontmatter!.company!}</p>
+        {_.chunk(props.data!.speakers!.edges!, 2).map((chunkedEdges) => (
+          <div className="columns">
+            {...chunkedEdges.map((edge) => (
+              <div className="column">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="media">
+                      <div className="media-left">
+                        <figure className="image is-128x128">
+                          <img src={edge.node!.frontmatter!.image!} alt={edge.node!.frontmatter!.fulllName!} />
+                        </figure>
+                      </div>
+                      <div className="media-content">
+                        <p className="title is-4">{edge.node!.frontmatter!.fulllName!}</p>
+                        <p className="subtitle is-6">{edge.node!.frontmatter!.jobTitle!}</p>
+                        <p className="subtitle is-6">{edge.node!.frontmatter!.company!}</p>
+                      </div>
+                    </div>
+                    <div className="content">
+                      {edge.node!.frontmatter!.description!}
+                    </div>
+                  </div>
                 </div>
               </div>
-
-              <div className="content">
-                {node.node!.frontmatter!.description!}
-              </div>
-            </div>
+            ))}
           </div>
         ))}
       </div>

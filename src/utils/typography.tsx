8ce744c2025423f76b17e7490eb3bd2b0e64cd3a
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-04-11 12:44:02 -0400

    changes to typography settings to more closely match style guide

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -4,7 +4,9 @@ import * as Typography from 'typography'
 const typography = new Typography({
   baseFontSize: '15px',
   baseLineHeight: 1.5,
-  bodyFontFamily: ['Roboto', 'Roboto Condensed', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Roboto Condensed', 'Roboto', 'Arial', 'sans-serif'],
+  bodyGray: 50,
+  boldWeight: 700,
   googleFonts: [
     {
       name: 'Roboto',
@@ -16,12 +18,19 @@ const typography = new Typography({
       name: 'Roboto Condensed',
       styles: [
         '400',
+        '700',
       ],
     },
   ],
-  headerFontFamily: ['Roboto', 'Roboto Condensed', 'Arial', 'sans-serif'],
-  scaleRatio: 5.2,
-    // See below for the full list of options.
+  headerFontFamily: ['Roboto Condensed', 'Roboto', 'Arial', 'sans-serif'],
+  headerGray: 50,
+  headerWeight: 700,
+  overrideStyles: (scale: Typography.TypographyOptions, rhythm: Typography.rhythm) => ({
+    body: {
+      letterSpacing: '.05em',
+    },
+  }),
+  scaleRatio: 2.5,
 })
 
 export default typography

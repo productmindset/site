91082828486670f35c217f79cd7f32dfba16d07c
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-05-07 17:32:27 -0400

    PMS-6 add analytics via segment.js (with amplitude as a destination in segment)

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,25 @@ module.exports = {
       }
     },
     `gatsby-transformer-remark`,
-    `gatsby-plugin-react-next`
+    `gatsby-plugin-react-next`,
+    {
+      resolve: `gatsby-plugin-segment-js`,
+      options: {
+          // your segment write key for your production environment
+          // when process.env.NODE_ENV === 'production'
+          // required; non-empty string
+          prodKey: `w7UUvSNSMjO6PEWIwMSDDUdWaEAKIWok`,
+
+          // if you have a development env for your segment account, paste that key here
+          // when process.env.NODE_ENV === 'development'
+          // optional; non-empty string
+          devKey: `pYHBLk0Cnzt02FS3keymuYeqSx22iGRR`,
+
+          // whether you want to include analytics.page()
+          // optional; boolean that defaults to true
+          // if false, then don't forget to manually add it to your codebase manually!
+          trackPage: true
+      }
+    },
   ]
 }

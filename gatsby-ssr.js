5c032b534ee83c87c20bd21f73a000004904b614
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-05-07 22:37:51 -0400

    fix issue in loading script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,16 +8,17 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   // use prod write key when in prod env, else use dev write key
   // note below, snippet wont render unless writeKey is truthy
   const writeKey = process.env.NODE_ENV === `production` ? prodKey : devKey
+  const trackPage = `analytics.page();`
 
   // Segment's minified snippet (version 4.1.0)
   const snippet = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.1.0";
     analytics.load('${writeKey}');
-    analytics.page();
+    ${trackPage}}}();
   `
 
   setHeadComponents([
     <script
-      key="plugin-segment"
+      key="segment"
       dangerouslySetInnerHTML={{ __html: snippet }}
     />
   ])

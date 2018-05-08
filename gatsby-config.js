0cb95b9b4dc8bcf66d12335fef05a46f1902333a
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-05-08 00:16:18 -0400

    fix quote / lint issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sentry`,
       options: {
-        dsn: 'https://61f7cd75771243b585e25962eef08863@sentry.io/1202850',
+        dsn: `https://61f7cd75771243b585e25962eef08863@sentry.io/1202850`,
         // Raven.js version
         version: `3.19.1`
       }

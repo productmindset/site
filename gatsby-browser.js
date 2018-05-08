cca53462cbe5fbc55ecbaa948076b61f9dc4cbab
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-05-07 22:16:06 -0400

    Removed segment gatsby plugin, manually use gatsby-ssr and gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,15 @@
+const { NODE_ENV = `development` } = process.env
+
+export const onRouteUpdate = ({ location }) => {
+  if (!window.analytics || typeof window.analytics.page !== `function`) {
+    if (NODE_ENV === `development`) {
+      console.warn(`Unable to locate analytics.js`)
+    }
+    return
+  }
+
+  window.analytics.page({
+    path: location.pathname,
+    url: location.href
+  })
+}

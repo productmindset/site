482a225b35a0159962199b6d8507e82c63b3f392
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-06-04 20:34:52 -0400

    Use Bloomer objects instead of Bulma classnames

--- src/templates/index-page.tsx
+++ src/templates/index-page.tsx
@@ -4,6 +4,16 @@ import * as _ from 'lodash'
 import * as PageComponent from '../components/PageComponent'
 import Img from 'gatsby-image'
 import { MarkdownRemark, MarkdownRemarkConnection, ImageSharp } from '../graphql-types'
+import {
+  Card,
+  CardContent,
+  CardImage,
+  Column,
+  Columns,
+  Container,
+  Section,
+  Title
+} from 'bloomer'
 
 interface HomePageTemplateLayoutProps extends Page.PageTemplateLayoutProps {
   data?: {
@@ -16,36 +26,37 @@ interface HomePageTemplateLayoutProps extends Page.PageTemplateLayoutProps {
 const IndexPageLayout: React.StatelessComponent<HomePageTemplateLayoutProps> = (props) => {
   return (
     <PageComponent.default {...props}>
-      <div className="section container" >
-        {_.chunk(props.data!.benefit!.edges!, 2).map((chunkedEdges, key) => (
-          <div className="columns" key={key}>
-            {...chunkedEdges.map((benefitEdge, benefitKey) => (
-              <div className="column" key={benefitKey}>
-                <div className="card has-text-centered">
-                  <br/>
-                  <div className="card-image is-64x64">
-                    <figure className="is-flex is-horizontally-centered">
-                      <Img className="image is-64x64 is-flex is-horizontally-centered"
-                        title={benefitEdge!.node!.frontmatter!.fullTitle!}
-                        alt={benefitEdge!.node!.frontmatter!.fullTitle!}
-                        resolutions={benefitEdge!.node!.imageFile!.childImageSharp!.resolutions!}
-                      />
-                    </figure>
+      <Container>
+        <Section>
+          {_.chunk(props.data!.benefit!.edges!, 2).map((chunkedEdges, key) => (
+            <Columns key={key}>
+              {...chunkedEdges.map((benefitEdge, benefitKey) => (
+                <Column key={benefitKey}>
+                  <Card className="has-text-centered">
+                    <br />
+                    <CardImage className="is-64x64">
+                      <figure className="is-flex is-horizontally-centered">
+                        <Img className="image is-64x64 is-flex is-horizontally-centered"
+                          title={benefitEdge!.node!.frontmatter!.fullTitle!}
+                          alt={benefitEdge!.node!.frontmatter!.fullTitle!}
+                          resolutions={benefitEdge!.node!.imageFile!.childImageSharp!.resolutions!}
+                        />
+                      </figure>
 
-                  </div>
-                  <div className="card-content is-centered">
-                  <div className="title is-4 is-centered">{benefitEdge!.node!.frontmatter!.fullTitle!}</div>
-                    <div>
-                      {benefitEdge!.node!.frontmatter!.description!}
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+                    </CardImage>
+                    <CardContent className="is-centered">
+                      <Title className="is-4 is-centered">{benefitEdge!.node!.frontmatter!.fullTitle!}</Title>
+                      <div>
+                        {benefitEdge!.node!.frontmatter!.description!}
+                      </div>
+                    </CardContent>
+                  </Card>
+                </Column>
+              ))}
+            </Columns>
+          ))}
+        </Section>
+      </Container>
     </PageComponent.default>
   )
 }

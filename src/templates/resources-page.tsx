f292fcc97d5ab404671699aa8d3dc1ec3ba5c023
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2018-06-18 10:51:02 -0400

    [PMS-12] filter resources

--- src/templates/resources-page.tsx
+++ src/templates/resources-page.tsx
@@ -3,19 +3,11 @@ import * as Page from './page'
 import * as _ from 'lodash'
 import * as PageComponent from '../components/PageComponent'
 import * as ResourceComponent from '../components/ResourceComponent'
-import Img from 'gatsby-image'
-import { MarkdownRemark, MarkdownRemarkConnection, ImageSharp } from '../graphql-types'
+import { MarkdownRemark, MarkdownRemarkEdge, MarkdownRemarkConnection, ImageSharp } from '../graphql-types'
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardHeaderTitle,
-  CardImage,
   Column,
   Columns,
   Container,
-  Section,
-  Title
 } from 'bloomer'
 
 interface ResourcePageTemplateLayoutProps extends Page.PageTemplateLayoutProps {
@@ -23,32 +15,65 @@ interface ResourcePageTemplateLayoutProps extends Page.PageTemplateLayoutProps {
     imageSharp?: ImageSharp
     markdownRemark?: MarkdownRemark
     resource?: MarkdownRemarkConnection
+    allResourceGroupsMarkdown?: {allResourceGroups: string[]},
   },
 }
 
-const ContactPageLayout: React.StatelessComponent<ResourcePageTemplateLayoutProps> = (props) => {
-  return (
-    <PageComponent.default {...props}>
-      <Container>
-        {_.chunk(props.data!.resource!.edges!, props.data!.markdownRemark!.frontmatter!.subColumns!)
-          .map((chunkedEdges, key) => (
-            <Columns key={key}>
-              {...chunkedEdges.map((resourceEdge, resourceKey) => (
-                <Column key={resourceKey}>
-                  <ResourceComponent.default node={resourceEdge!.node!} />
-                </Column>
-              ))}
-            </Columns>
-          ))}
-      </Container>
-    </PageComponent.default>
-  )
+interface ResourcePageLayoutState {
+  resourceEdges?: Array<(MarkdownRemarkEdge | null)> | null /** A list of edges. */
 }
 
+class ResourcePageLayout extends React.Component<ResourcePageTemplateLayoutProps, ResourcePageLayoutState> {
+  constructor(props: ResourcePageTemplateLayoutProps) {
+    super(props)
+    this.state = { resourceEdges: props.data!.resource!.edges }
+  }
+
+  handleFilter = (filter: string) => () => {
+    this.setState({
+      resourceEdges: this.props.data!.resource!.edges!.filter((edge) =>
+        edge!.node!.frontmatter!.resourceGroup === filter),
+    })
+  }
+
+  clearFilter = () => () => {
+    this.setState({
+      resourceEdges: this.props.data!.resource!.edges,
+    })
+  }
+
+  render() {
+    return (
+      <PageComponent.default {...this.props}>
+        <Container>
+        Filter these resources by:<br/>
+        <a onClick={this.clearFilter()}>All</a>
+        {this.props.data!.allResourceGroupsMarkdown!.allResourceGroups!.map((resourceGroup, index) => (
+          <React.Fragment key={index}>
+            &nbsp;|&nbsp;<a onClick={this.handleFilter(resourceGroup)}>{resourceGroup}</a>
+          </React.Fragment>
+        ))}
+        {_.chunk(this.state.resourceEdges!, this.props.data!.markdownRemark!.frontmatter!.subColumns!)
+            .map((chunkedEdges, key) => (
+              <Columns key={key}>
+                {...chunkedEdges.map((resourceEdge, resourceKey) => (
+                  <Column isSize={4} key={resourceKey}>
+                    <ResourceComponent.default node={resourceEdge!.node!} />
+                  </Column>
+                ))}
+              </Columns>
+            ))}
+        </Container>
+      </PageComponent.default>
+    )
+  }
+}
 export const ResourcesPageTemplateQuery = graphql`
   query ResourcesPageTemplateQuery($slug: String!, $heroImageSlug: String) {
     ...PageQueryFragment
-    ...PageQueryFragment
+    allResourceGroupsMarkdown: allMarkdownRemark(filter: {frontmatter: {type: {eq: "resource"}}}, limit: 1000) {
+      allResourceGroups: distinct(field: frontmatter___resourceGroup)
+    }
     resource: allMarkdownRemark(
       filter: {frontmatter: {type: {eq: "resource"}, active: {eq: true}}},
       sort: {fields: [frontmatter___sortOrder], order: ASC}) {
@@ -63,7 +88,8 @@ export const ResourcesPageTemplateQuery = graphql`
           }
         }
         frontmatter {
-          resourceType
+          resourceGroup,
+          resourceType,
           url,
           buttonText,
           buttonFAIcon,
@@ -77,4 +103,4 @@ export const ResourcesPageTemplateQuery = graphql`
   }
 `
 
-export default ContactPageLayout
+export default ResourcePageLayout

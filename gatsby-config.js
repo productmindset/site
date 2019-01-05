9b05e8bb233f59bd87428b3669974749dc3c1061
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2019-01-05 17:12:57 -0500

    updated yarn and raven versions

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
       options: {
         dsn: `https://61f7cd75771243b585e25962eef08863@sentry.io/1202850`,
         // Raven.js version
-        version: `3.19.1`
+        version: `3.27.0`
       }
     },
     {

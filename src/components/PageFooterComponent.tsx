9b0e21a08bf2f41ddebbcd350298dbd66198143a
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2019-01-05 14:47:29 -0500

    linting fixes

--- src/components/PageFooterComponent.tsx
+++ src/components/PageFooterComponent.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-import {
-  Container,
-  Footer,
-  Section
-} from 'bloomer'
+import { Container, Footer, Section } from 'bloomer'
 
 export class PageFooter extends React.Component<{}, {}> {
-
   render() {
     return (
       <div>
@@ -16,73 +11,75 @@ export class PageFooter extends React.Component<{}, {}> {
           <Container>
             <div className="has-background-light">
               <div className="has-text-centered">
-                <p className="is-size-1">
-                  Find out the latest
-              </p>
+                <p className="is-size-1">Find out the latest</p>
                 <p id="subscribe-now">
                   Subscribe now to receive news and updates about next events,
                   free resources, exclusive trainings and workshops.
-              </p>
+                </p>
               </div>
-              <form name="subscribe" method="POST" data-netlify="true" netlify-honeypot="test-field">
+              <form
+                name="subscribe"
+                method="POST"
+                data-netlify="true"
+                netlify-honeypot="test-field"
+              >
                 <input type="hidden" name="subscribe" value="subscribe" />
                 <div className="is-hidden">
-                  <label>Don’t fill this out if you're human
-                <input name="test-field" />
+                  <label>
+                    Don’t fill this out if you're human
+                    <input name="test-field" />
                   </label>
                 </div>
 
                 <div className="field is-grouped">
                   <div className="control has-icons-left has-icons-right is-expanded">
-                    <input className="input" type="email" name="email" placeholder="Email" aria-label="email to subscribe" aria-labelledby="subscribe-now" />
+                    <input
+                      className="input"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      aria-label="email to subscribe"
+                      aria-labelledby="subscribe-now"
+                    />
                     <span className="icon is-small is-left">
-                      <i className="fas fa-envelope"></i>
+                      <i className="fas fa-envelope" />
                     </span>
                   </div>
                   <div className="control">
-                    <button type="submit" className="button is-primary">Subscribe</button>
+                    <button type="submit" className="button is-primary">
+                      Subscribe
+                    </button>
                   </div>
                 </div>
-
               </form>
             </div>
           </Container>
         </Section>
         <Footer className="has-text-white">
           <p>
-            <Link className="has-text-white"
-              to={'/'}
-            >
+            <Link className="has-text-white" to={'/'}>
               HOME
             </Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link className="has-text-white"
-              to={'/workshops/'}
-            >
+            <Link className="has-text-white" to={'/workshops/'}>
               LIVE WORKSHOPS
             </Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link className="has-text-white"
-              to={'/resources/'}
-            >
+            <Link className="has-text-white" to={'/resources/'}>
               ONLINE RESOURCES
             </Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link className="has-text-white"
-              to={'/faq/'}
-            >
+            <Link className="has-text-white" to={'/faq/'}>
               SUPPORT
             </Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link className="has-text-white"
-              to={'/contact/'}
-            >
+            <Link className="has-text-white" to={'/contact/'}>
               CONTACT
             </Link>
           </p>
           <p className="is-size-7">
             Copyright &copy; Product Mindset, Powered by &nbsp;
-                <a className="has-text-white" href="https://www.3pillarglobal.com">
+            <a className="has-text-white" href="https://www.3pillarglobal.com">
               <u>3Pillar Global</u>
             </a>
           </p>

5cd81fe9ffda48ae8fb147d184b5e19295cee52e
Derek Tiffany <derek.tiffany@3pillarglobal.com>
2019-03-12 14:54:10 -0400

    added larger logo to resize better

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `static/images/included/favicon.png`, // This path is relative to the root of the site.
+        icon: `static/images/included/logo_globe_only_610x610.png`, // This path is relative to the root of the site.
         include_favicon: true // Include favicon
       }
     },
